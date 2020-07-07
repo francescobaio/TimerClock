778ef05594cf8367f23dc7f5ef99973912f652d9
francescobaiocchi <frappabaio@libero.it>
2020-07-07 16:42:52 +0200

    Creazione del codice per la visualizzazione di ora e data in vari formati.

---
+++ datetime.cpp
@@ -0,0 +1,28 @@
+#include "datetime.h"
+
+
+void DateTime::changeDateFormat(){
+    dateCount++;
+    setFormat();
+}
+
+void DateTime:: changeTimeFormat(){
+    timeCount++;
+    setFormat();
+}
+
+
+void DateTime::setFormat(){
+    if(dateCount % 3 == 0 && timeCount % 2 == 0)
+        setDisplayFormat("dd/MM/yyyy hh:mm");
+    else if(dateCount % 3 == 0 && timeCount % 2 == 1)
+        setDisplayFormat("dd/MM/yyyy hh:mm AP");
+    else if(dateCount % 3 == 1 && timeCount % 2 == 0)
+        setDisplayFormat("dd.MM.yyyy hh:mm");
+    else if(dateCount % 3 == 1 && timeCount % 2 == 1)
+        setDisplayFormat("dd.MM.yyyy hh:mm AP");
+    else if(dateCount % 3 == 2 && timeCount % 2 == 0)
+        setDisplayFormat("d MMMM yyyy hh:mm");
+    else if(dateCount % 3 == 2 && timeCount % 2 == 1)
+        setDisplayFormat("d MMMM yyyy hh:mm AP");
+}
---
+++ datetime.h
@@ -0,0 +1,28 @@
+#ifndef DATETIME_H
+#define DATETIME_H
+
+#include <QDateTimeEdit>
+#include<QDateTime>
+
+
+
+class DateTime :  public QDateTimeEdit,public QDateTime
+{
+    Q_OBJECT
+
+public:
+    DateTime(QWidget * Q) : QDateTimeEdit(Q){
+        QDateTimeEdit::setDateTime(currentDateTime());
+    }
+
+public slots:
+    void changeTimeFormat();
+    void changeDateFormat();
+
+private:
+      void setFormat();
+      int dateCount = 0;
+      int timeCount = 0;
+};
+
+#endif // DATETIME_H
