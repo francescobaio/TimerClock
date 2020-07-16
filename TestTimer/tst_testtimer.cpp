964f693b2f761a33de341d6d96276b6bec552f49
francescobaiocchi <frappabaio@libero.it>
2020-07-16 12:09:10 +0200

    Test che verifica il corretto comportamento della classe Timer.

---
+++ TestTimer/tst_testtimer.cpp
@@ -0,0 +1,40 @@
+#include <QtTest>
+#include <QCoreApplication>
+#include"../timer.h"
+#include"../timer.cpp"
+#include"../clock.h"
+#include"../clock.cpp"
+#include"../chrono.h"
+#include"../chrono.cpp"
+
+// add necessary includes here
+
+class TestTimer : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestTimer(){}
+    ~TestTimer(){}
+
+private slots:
+    void test_timer();
+
+};
+
+void TestTimer::test_timer()
+{
+    Timer t(0,0,10);
+    Clock c(&t);
+    t.setState();
+    QCOMPARE(t.getSecond(),0);
+    QCOMPARE(c.getSecond(),0);
+}
+
+
+
+
+
+QTEST_MAIN(TestTimer)
+
+#include "tst_testtimer.moc"
